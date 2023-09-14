4add0da8a192aca292721282e7f69a4198be76cd
DylanJoo <dylanjootw@gmail.com>
2023-09-14 11:18:26 +0800

    download scripts

--- train_text2text.sh
+++ train_text2text.sh
@@ -1,22 +1,21 @@
-export CUDA_VISIBLE_DEVICES=2
-COLLECTION_SIM=/tmp2/trec/pds/data/collection/collection_sim.jsonl
+# export CUDA_VISIBLE_DEVICES=2
 
 python3 ../text2text/train.py \
     --model_name_or_path t5-base \
     --config_name t5-base \
     --tokenizer_name t5-base \
-    --train_file $COLLECTION_SIM \
+    --train_file data/trec-pds.train.product2query.jsonl \
     --max_src_length 256  \
     --max_tgt_length 32 \
-    --do_train \
-    --do_eval \
-    --num_train_epochs 2 \
+    --output_dir /models/t5-base-product2query \
+    --do_train --do_eval \
     --save_strategy steps \
-    --save_steps 100 \
-    --output_dir /tmp2/trec/pds/models/t5-base-desc2title \
-    --eval_steps 2500 \
+    --max_steps 10000 \
+    --save_steps 2500 \
+    --eval_steps 500 \
     --evaluation_strategy steps \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 4 \
-    --learning_rate 1e-4
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 \
+    --learning_rate 1e-4 \
+    --template "summarize: title: {0} description: {1}"
 
