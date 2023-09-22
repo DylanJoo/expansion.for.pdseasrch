280718b2b292a1c70e273164710d7a7f79f656cb
DylanJoo <dylanjootw@gmail.com>
2023-09-22 10:13:26 +0800

    add infer scripts

--- infer_text2text.sh
+++ infer_text2text.sh
@@ -1,18 +1,21 @@
 FILE=data/corpus.jsonl
-FILE_OUT=${FILE/data/results}
-MODEL=models/t5-base-product2query/t5-base-product2query/old
-MODEL=models/t5-base-product2query/t5-base-product2query
+# MODEL=models/t5-base-product2query
+# MODEL=models/t5-base-product2query/old
 mkdir -p results/
 
-python text2text/generate.py \
-    --collection $FILE \
-    --model_name $MODEL \
-    --tokenizer_name t5-base \
-    --do_sample \
-    --top_k 10 \
-    --batch_size 40 \
-    --max_src_length 512 \
-    --max_tgt_length 64 \
-    --num_return_sequences 10  \
-    --output_jsonl ${FILE_OUT/jsonl/pred.jsonl}\
-    --device cuda
+for ckpt in 10000 20000 30000 40000;do
+    MODEL=models/t5-base-product2query/checkpoint-${ckpt}
+    FILE_OUT=results/corpus.ckpt-${ckpt}.pred.jsonl
+    python text2text/generate.py \
+        --collection data/corpus.jsonl \
+        --model_name $MODEL \
+        --tokenizer_name t5-base \
+        --do_sample \
+        --top_k 10 \
+        --batch_size 32 \
+        --max_src_length 512 \
+        --max_tgt_length 64 \
+        --num_return_sequences 10  \
+        --output_jsonl $FILE_OUT \
+        --device cuda
+done
