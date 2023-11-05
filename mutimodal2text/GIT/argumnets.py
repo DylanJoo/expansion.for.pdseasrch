e67a505c928528ece3b27a545dbdfc1dab668093
DylanJoo <dylanjootw@gmail.com>
2023-11-05 10:52:43 +0800

    [m2t] initialize

---
+++ mutimodal2text/GIT/argumnets.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from dataclasses import dataclass, field
+from typing import Optional, Union
+from transformers import Seq2SeqTrainingArguments
+
+@dataclass
+class ModelArgs:
+    model_name_or_path: Optional[str] = field(default=None)
+    config_name: Optional[str] = field(default=None)
+    tokenizer_name: Optional[str] = field(default=None)
+    use_fast_tokenizer: bool = field(default=True)
+    datacollator: str = field(default='product2query')
+
+@dataclass
+class DataArgs:
+    train_file: Optional[str] = field(default=None)
+    eval_file: Optional[str] = field(default=None)
+    max_src_length: int = field(default=256)
+    max_tgt_length: int = field(default=16)
+
+@dataclass
+class TrainArgs(Seq2SeqTrainingArguments):
+    do_train: bool = field(default=False)
+    do_eval: bool = field(default=False)
+    max_steps: int = field(default=-1)
+    save_steps: int = field(default=5000)
+    eval_steps: int = field(default=2500)
+    evaluation_strategy: Optional[str] = field(default='steps')
+    template: Optional[str] = field(default="{0} {1}")
