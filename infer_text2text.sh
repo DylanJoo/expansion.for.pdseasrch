0521cfe9778b098ef7754cb5d936f2aeea95091b
DylanJoo <dylanjootw@gmail.com>
2023-11-05 12:12:29 +0800

    update infer codes

--- infer_text2text.sh
+++ infer_text2text.sh
@@ -3,11 +3,11 @@ mkdir -p results/
 
 for ckpt in 10000 15000 20000;do
     MODEL=models/t5-base-product2query/checkpoint-${ckpt}
-    FILE_OUT=results/corpus.ckpt-${ckpt}.pred.jsonl
+    FILE_OUT=results/corpus.t5-base-product2query-${ckpt}.pred.jsonl
     python text2text/generate.py \
         --collection data/corpus.jsonl \
         --model_name $MODEL \
-        --model_hf_name google/t5-v1_1-base \
+        --model_hf_name google/t5-base \
         --do_sample \
         --top_k 10 \
         --batch_size 40 \
