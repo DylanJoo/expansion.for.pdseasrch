bb1c69bb03fc400122189b2aa2a52ac19537ced7
JHJu <39515279+DylanJoo@users.noreply.github.com>
2023-11-11 15:28:15 +0800

    Splade (#8)
    * [splade] init
    
    * rename splde and add indexing codes
    
    * splade

--- search.sh
+++ search.sh
@@ -2,7 +2,8 @@ mkdir -p runs
 
 prefix=trec-pds-expanded
 model=t5-base-product2query
-for ckpt in 10000 15000 20000 25000 30000;do
+model=ckpt
+for ckpt in 10000 15000 20000;do
     python3 retrieval/bm25_search.py \
         --query data/qid2query-dev-filtered.tsv \
         --output runs/dev-bm25-title.prod2query.${ckpt}.trec \
